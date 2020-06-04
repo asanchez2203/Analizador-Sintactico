678c6e48c31c65761272f83b2adfba024556d3cd
andresarellano56 <andresarellanom56@gmail.com>
2020-06-04 17:05:17 -0500

    matrix

--- src/MatrizPredictiva/MatrizPredictiva.java
+++ src/MatrizPredictiva/MatrizPredictiva.java
@@ -13,15 +13,38 @@ public class MatrizPredictiva {
     }
 
     private void setValues(){
-        matrizPredictiva[0][0] = 1; //Interseccion inicio-begin
+        matrizPredictiva[0][0] = 1; //Interseccion programa-inicio
+
         matrizPredictiva[1][1] = 2; //Interseccion begin-program
 
-        matrizPredictiva[2][2] = 4; // Interseccion stmt_list - id
-        matrizPredictiva[2][3] = 5; // Interseccion stmt - id
-        matrizPredictiva[2][4] = 6; // Interseccion expression - id
-        matrizPredictiva[2][6] = 10; // Interseccion primary - id
+        matrizPredictiva[2][2] = 2; //Interseccion end-stmt_list
 
-        matrizPredictiva[3][6] = 10; // Interseccion primary - id
+        matrizPredictiva[3][2] = 4; // Interseccion id - stmt_list
+        matrizPredictiva[3][3] = 5; // Interseccion id - stmt
+        matrizPredictiva[3][4] = 6; // Interseccion id - expression
+        matrizPredictiva[3][6] = 10; // Interseccion id - primary
+
+        matrizPredictiva[5][5] = 8; // Interseccion ; - primarytail
+
+        matrizPredictiva[6][4] = 6; // Interseccion ( - expression
+        matrizPredictiva[6][6] = 9; // Interseccion ( - primary
+
+        matrizPredictiva[7][5] = 8; // Interseccion ) - primarytail
+
+        matrizPredictiva[8][4] = 6; // Interseccion intliteral - expression
+        matrizPredictiva[8][6] = 11; // Interseccion intliteral - primary
+
+        matrizPredictiva[9][4] = 6; // Interseccion realliteral - expression
+        matrizPredictiva[9][6] = 12; // Interseccion realliteral - primary
+
+        matrizPredictiva[10][5] = 7; // Interseccion + - primarytail
+        matrizPredictiva[10][7] = 13; // Interseccion + - aritop
+
+        matrizPredictiva[11][5] = 7; // Interseccion - - primarytail
+        matrizPredictiva[11][7] = 14; // Interseccion - - aritop
+
+        matrizPredictiva[12][5] = 7; // Interseccion * - primarytail
+        matrizPredictiva[12][7] = 15; // Interseccion * - aritop
     }
 
     public int[][] getMatrizPredictiva(){
